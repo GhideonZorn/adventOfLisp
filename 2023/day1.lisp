24151a38b17a70336f616b33e63b557c8365e03c
Ghislain Bonnard <ghislain.bonnard@epita.fr>
2023-12-02 14:55:24 +0100

    refactor(day1.lisp): clean some useless lines

--- 2023/day1.lisp
+++ 2023/day1.lisp
@@ -1,6 +1,3 @@
-(defun get-line (stream-in)
-  (read-line stream-in nil))
-
 (defun get-calibration-value (line)
   (let ((fd -1)
         (ld -1)
@@ -15,11 +12,10 @@
 
           ;; Set to nil for part 1.
           (t
-           (let ((match_res (return-if-match
-                            line
-                             pos
-                             '("one" "two" "three" "four" "five" "six" "seven" "eight" "nine")
-                             '(1 2 3 4 5 6 7 8 9))))
+           (let ((match_res
+                   (return-if-match line pos
+                        '("one" "two" "three" "four" "five" "six" "seven" "eight" "nine")
+                        '(1 2 3 4 5 6 7 8 9))))
              (cond ((not (eq match_res nil))
                         (if (eq fd -1)
                             (setf fd match_res)
@@ -30,6 +26,10 @@
           (setf pos (+ pos 1)))
     (+ (* fd 10) ld)))
 
+
+;; loop over the string line start at start. If it matches
+;; an string element of string_list, it returns the
+;; associated item of item_list (any type)
 (defun return-if-match (line start string_list item_list)
   (let ((pos 0))
     (loop for str in string_list do
@@ -42,15 +42,14 @@
     )))
 
 (defun parse-line (stream-in)
-  (let ((line (get-line stream-in)))
+  (let ((line (read-line stream-in nil)))
         (if (null line)
           0
           (+ (get-calibration-value line) (parse-line stream-in)))
         ))
 
 (defun compute-result (file)
-  (let ((calibration-sum 0))
     (with-open-file (file-stream file)
-      (setf calibration-sum (parse-line file-stream)))))
+      (parse-line file-stream)))
 
 (format T "Day 1: ~d~%" (compute-result "day1.txt"))
